6b36dab7e39d3e55013cfcabac4a4b5097c403c7
MarvinSoft <marvin.erazo@espoch.edu.ec>
2021-08-24 11:04:47 -0500

    primer commit

---
+++ README.md
@@ -0,0 +1,30 @@
+Algunas acotaciones para nombres:
+    *Nombres especificos para cada funcion y archivo que vayan a crear:
+    El nombre debe ser acorde a su funcion, la aplicacion esta dividida en 3 capas
+    Colocar cada archivo donde corresponde, si hay dudas escriban en el chat
+    ejm: insertar.php
+
+    *Nombres para variables,id,name:
+    Los nombres de las variables se definen de acuerdo a su tipo y funcion
+    Formularios: id='frmEjemplo' name='frmEjemplo'
+    Lables: id='lblEjemplo' name='lblEjemplo'
+    Inputs: id='inEjemplo' name='inEjemplo'
+
+    *Patrones de diseño:
+    El diseño queda acargo de cada persona, tanto para las tablas como paginas o alerts boostrap
+    Deben ver que sea acorde al tema, preferible colores azules no muy oscuros con blanco de fondo
+    la letra y demas igual queda a gusto de cada quien.
+
+    *Base de datos:
+    La base de datos la tendran todos localmente, al momento de hacer los validaciones cada uno veriricara que funcione
+    en su base de datos.
+
+    *Git:
+    Se trabajara con github, en una sola rama para evitar inconvenientes, no modificar los archivos que no les corresponda
+    se dividira el trabajo conforme cada quien se encargue de un o dos archivos.
+    Actuualizar tu repositorio local constantemente, se haran cambios cada 1 hora
+    --git clone 
+    --git add .
+
+    *Imagenes:
+    Ubicar Imagenes
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Venta de Pescados de Alta Mar en Ecuador al por mayor y menos">
+    <meta name="keywords" content="Pescaderia,Venta de pescado,Pesca,Mar">
+    <meta name="author" content="AI Grupo: Pescaderia" />
+    <meta name="copyright" content="Derechos de Autor: Grupo Pescaderia" />
+    <title>FisherMan</title>
+
+    <link rel="shortcut icon" href="Imagenes/icono.png" type="image/x-icon">
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <link rel="stylesheet" href="css/estilos.css">
+
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- De aqui para abajo van los estilos y funciones que le dan estilo de la pagina -->
+    <!-- Tambien pueden agregar fuentes de googlefont-->
+    <link rel="stylesheet" href="Vista/Estilos/index.css">
+    <script src="Vista/Funciones/index.js"></script>
+    <!-- Todo lo que se diriija a un metodo POST se hace en Controlado -->
+    <!-- La Carpeta modelo debe tener las funciones de mostrar,insetar,editar y eliminar ademas de los reportes-->
+
+</head>
+<body>
+
+    
+</body>
+</html>
